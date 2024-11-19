5f75733e574faf8a312511a51717883af28aeeb2
Jake <jake.ar@paymentop.com>
2024-11-19 12:53:28 +0200

    fix: strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
@@ -6,5 +6,14 @@ import { RouterOutlet } from '@angular/router'
   standalone: true,
   imports: [RouterOutlet],
   template: `<router-outlet />`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  public readonly user = {
+    name: 'Jon',
+  }
+
+  getName() {
+    return this.user.name
+  }
+}
